c102bd4019b785d7b0c49304a9c1932748fd87e5
mguppy <mguppy34@gmail.com>
2021-06-25 17:01:12 -0500

    updated pseudocode in js file

---
+++ assets/logic.js
@@ -0,0 +1,27 @@
+//TO DOs in HTML: create one blank div for the question and then create 4 buttons and then reuse for the choices
+
+//New function for each event
+
+//User clicks button to "Start Quiz"
+//Event listener needed
+
+//When button is clicked, the timer starts counting down and is displayed in the top right corner
+//Event listener to start timer and display timer on event click
+//setInterval function needed (timer)
+
+
+//When button is clicked, the first question appears by using textContent to fill in blank div and using textContent to fill in blank buttons
+//Event listener to display first question
+//set up objects for each question and choices
+//Set up 5 objects for all 5 questions (question, choiceArray (will store 4 choices), correctAnswer)
+//Inside each of these objects, create an array for all choices for this question
+
+//Once user selects answer to first question, the user is told if answer is right or wrong
+//Event listener for each button that the user could select
+//You can pass in the text in the button as an argument to the function created for the event listener
+//If statement saying if user's selection matches "correctAnswer", then add point to score
+//Else, decrease time by 5 seconds (5000 milliseconds)
+
+//When all questions are answered or the time reaches 0, game is over
+//Display message "All done" and have text inputs for user to enter in their initials
+//Store their initials and score to local storage
